cbff25a9e1e1ae4a8f71e8e171af0b651c290422
Avsheshsavani <avshesh.savani7@gmail.com>
2022-04-15 13:37:53 +0530

    comment added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
-import {Route,Switch} from 'react-router'
+import { Route, Switch } from "react-router";
 import Aboutus from "./pages/Aboutus";
 import Gallary from "./pages/Gallary";
 import Contacts from "./pages/Contacts";
-import Routes from './Routes';
+import Routes from "./Routes";
 import Services from "./pages/Services";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
-
+//commite added
 function App() {
   return (
-   <>
-   <Switch>
-
-   <Route exact path="/" component={Routes}></Route>
-   <Route path="/home" component={Home}></Route>
-   <Route path="/about" component={Aboutus}></Route>
-   <Route path="/contact" component={Contacts}></Route>
-   <Route path="/gallary" component={Gallary}></Route>
-   <Route path="/services" component={Services}></Route>
-   <Route path="/error" component={Error}></Route>
-   </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Routes}></Route>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/about" component={Aboutus}></Route>
+        <Route path="/contact" component={Contacts}></Route>
+        <Route path="/gallary" component={Gallary}></Route>
+        <Route path="/services" component={Services}></Route>
+        <Route path="/error" component={Error}></Route>
+      </Switch>
     </>
   );
 }
